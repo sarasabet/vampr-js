e2bfd80154c8bc4fc92d88a2d3df97038f56f4b2
Sara Sabet <sara.ilbeigi@gmail.com>
2021-09-03 21:24:17 +0000

    complete functions

--- vampire.js
+++ vampire.js
@@ -10,35 +10,48 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+   return  this.offspring.length
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let number = 0;
+    let vamps = this;
 
+    while (vamps.creator) {
+      vamps = vamps.creator;
+      number++;
+    }
+    return number;
   }
+
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (
+      vampire.numberOfVampiresFromOriginal >= this.numberOfVampiresFromOriginal
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
-
   // Returns the closest common ancestor of two vampires.
   // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
   }
-}
+
 
 module.exports = Vampire;
 
